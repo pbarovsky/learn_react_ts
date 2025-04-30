0f8b6a5c2b27026b138c891e4c12b62beb0db9fc
Pavel Barovsky <pbarovsky@mail.ru>
2025-04-30 16:42:46 +0300

    final V backend express

---
+++ employees/routes/employees.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const { auth } = require("../middleware/auth");
+const {
+  all,
+  add,
+  remove,
+  edit,
+  employee,
+} = require("../controllers/employees");
+const e = require("express");
+
+// api/employess
+router.get("/", auth, all);
+
+// /api/employees/:id
+router.get("/:id", auth, employee);
+
+// /api/employees/add
+router.post("/add", auth, add);
+
+// /api/employees/remove/:id
+router.post("/remove/:id", auth, remove);
+
+// /api/employees/edit/:id
+router.put("/edit/:id", auth, edit);
+
+module.exports = router;
